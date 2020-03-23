b1b56342d65647626967763ccb7663049963c8e4
Adrian Li <li.adrianmc@gmail.com>
2020-03-22 21:35:40 -0400

    enhance money legoooooooosss

--- packages/money-legos/index.ts
+++ packages/money-legos/index.ts
@@ -1,20 +1,13 @@
-import makerProxyRegistryAbi from "../smart-contracts/test/abi/ProxyRegistry.json";
-import { dedgeProxyFactoryAddress } from "../smart-contracts/build/DeployedAddresses.json";
-import dedgeProxyFactoryDef from "../smart-contracts/build/DedgeProxyFactory.json";
+import maker from "./maker";
+import dedge from "./dedge";
+import erc20 from "./erc20";
+
+// note: a lego is just a Javascript object with two fields: address and abi
 
 const legos = {
-  maker: {
-    proxyRegistry: {
-      address: "0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4",
-      abi: makerProxyRegistryAbi,
-    },
-  },
-  dedge: {
-    proxyRegistry: {
-      address: dedgeProxyFactoryAddress,
-      abi: dedgeProxyFactoryDef.abi,
-    },
-  },
+  maker,
+  dedge,
+  erc20,
 };
 
 export default legos;
