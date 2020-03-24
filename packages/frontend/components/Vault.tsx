60e8d7c1de6e87299246a8f8736544680bc32b4d
Adrian Li <li.adrianmc@gmail.com>
2020-03-24 03:54:04 -0400

    add collateral and debt data for vault

--- packages/frontend/components/Vault.tsx
+++ packages/frontend/components/Vault.tsx
@@ -1,10 +1,22 @@
 import { Card, Flex, Box, Heading, Button } from "rimble-ui";
 
-const Vault = ({ vault }) => (
-  <>
-    <div>Vault ID: {vault.id}, Collateral: {vault.collateral}</div>
-    {/* <Button size={"small"}>Transfer position to Compound</Button> */}
-  </>
-);
+import ContractsContainer from "../containers/Contracts";
+import useVaultBalance from "../hooks/useVaultBalance";
+
+const Vault = ({ vault }) => {
+  const { contracts } = ContractsContainer.useContainer();
+  const [collateral, debt] = useVaultBalance(contracts, vault.id);
+
+  return (
+    <>
+      <div>
+        Vault ID: {vault.id},
+         Collateral: {collateral} {vault.collateral},
+         Debt: {debt} DAI
+      </div>
+      {/* <Button size={"small"}>Transfer position to Compound</Button> */}
+    </>
+  );
+};
 
 export default Vault;
