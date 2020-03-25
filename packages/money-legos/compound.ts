cd9aa89a619a5376d45d314d4a3cbd2000876546
Adrian Li <li.adrianmc@gmail.com>
2020-03-25 01:09:04 -0400

    add compound comptroller to lego

---
+++ packages/money-legos/compound.ts
@@ -0,0 +1,10 @@
+import ComptrollerDef from "../smart-contracts/build/IComptroller.json";
+
+const compound = {
+  comptroller: {
+    address: "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b",
+    abi: ComptrollerDef.abi,
+  },
+};
+
+export default compound;
--- packages/money-legos/index.ts
+++ packages/money-legos/index.ts
@@ -3,6 +3,7 @@ import dedge from "./dedge";
 import erc20 from "./erc20";
 import dappsys from "./dappsys";
 import uniswap from "./uniswap";
+import compound from "./compound";
 
 // note: a lego is just a Javascript object with two fields: address and abi
 
@@ -12,6 +13,7 @@ const legos = {
   erc20,
   dappsys,
   uniswap,
+  compound,
 };
 
 export default legos;
