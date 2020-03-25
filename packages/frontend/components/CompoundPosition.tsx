f171b3917d4dad005e2b7985b63351489c78a593
Adrian Li <li.adrianmc@gmail.com>
2020-03-25 01:49:40 -0400

    tweak UI

--- packages/frontend/components/CompoundPosition.tsx
+++ packages/frontend/components/CompoundPosition.tsx
@@ -6,11 +6,18 @@ const CompoundPosition = () => {
   const [markets] = useCompoundMarkets();
   const [enterMarkets] = useEnterCompoundMarkets();
   console.log("markets", markets);
+  if (!markets || markets.length < 2) {
+    return (
+      <Card>
+        <Heading as="h2">Compound Position</Heading>
+        <Button onClick={enterMarkets}>Enter Markets</Button>
+      </Card>
+    );
+  }
 
   return (
     <Card>
       <Heading as="h2">Compound Position</Heading>
-      <Button onClick={enterMarkets}>Enter Markets</Button>
       <h4>Current Position</h4>
       <h4>Swap Debt</h4>
     </Card>
