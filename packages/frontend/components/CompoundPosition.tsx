ab1675d51db3c41032b72641769e09d9be666a15
Adrian Li <li.adrianmc@gmail.com>
2020-03-25 01:24:11 -0400

    add ability to retrieve entered markets

--- packages/frontend/components/CompoundPosition.tsx
+++ packages/frontend/components/CompoundPosition.tsx
@@ -1,11 +1,16 @@
 import { Card, Flex, Box, Heading, Button } from "rimble-ui";
+import useCompoundMarkets from "../hooks/useCompoundMarkets";
 
-const CompoundPosition = () => (
-  <Card>
-    <Heading as="h2">Compound Position</Heading>
-    <h4>Current Position</h4>
-    <h4>Swap Debt</h4>
-  </Card>
-);
+const CompoundPosition = () => {
+  const [markets] = useCompoundMarkets();
+  console.log("markets", markets);
+  return (
+    <Card>
+      <Heading as="h2">Compound Position</Heading>
+      <h4>Current Position</h4>
+      <h4>Swap Debt</h4>
+    </Card>
+  );
+};
 
 export default CompoundPosition;
