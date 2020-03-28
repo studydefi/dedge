87f75286a177c7bfbf491adf5038c2c21c0b511f
Kendrick Tan <kendricktan0814@gmail.com>
2020-03-28 23:14:18 +1000

    fixed cether view

--- packages/money-legos/compound.ts
+++ packages/money-legos/compound.ts
@@ -7,6 +7,7 @@ import networkIds from './networks'
 
 
 const compound = {
+  cTokenAbi: CTokenAbi,
   comptroller: {
     address: {
       [networkIds.mainnet]: "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b",
