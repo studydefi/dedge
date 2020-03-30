550190b65412745105b8520e3f9105d0097b2698
Kendrick Tan <kendricktan0814@gmail.com>
2020-03-30 22:51:13 +1000

    instadapp stuff

--- packages/money-legos/instadapp.ts
+++ packages/money-legos/instadapp.ts
@@ -1,4 +1,5 @@
 import instaRegistryAbi from "./abi/instadapp/instaregistry.json";
+import instaDssProxyActionsAbi from "./abi/instadapp/instaDssProxyActions.json"
 
 import networkIds from "./networks";
 
@@ -7,6 +8,12 @@ const instadapp = {
     abi: instaRegistryAbi,
     address: {
       [networkIds.mainnet]: "0x498b3BfaBE9F73db90D252bCD4Fa9548Cd0Fd981"
+    }
+  },
+  instaDssProxyActions: {
+    abi: instaDssProxyActionsAbi,
+    address: {
+      [networkIds.mainnet]: "0x797f232C549026790E8707725DDA76DA70256fd6"
     }
   }
 };
