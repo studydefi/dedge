5e805a070f9c71f64e02e25edd1eec00e7e61d8a
Adrian Li <li.adrianmc@gmail.com>
2020-03-31 03:13:17 -0400

    sort out makerDAO import modal

--- packages/frontend/features/import-vault/FromMakerDAO.tsx
+++ packages/frontend/features/import-vault/FromMakerDAO.tsx
@@ -1,15 +1,21 @@
-import { Box, Modal, Button, Heading, Card } from "rimble-ui";
-
 import { useState } from "react";
+import { Box, Modal, Text, Button, Heading, Card, EthAddress } from "rimble-ui";
+
+// components
 import Select from "../../components/Select";
 import { ModalBottom, ModalCloseIcon } from "../../components/Modal";
+
+// containers
+import ConnectionContainer from "../../containers/Connection";
 import DACProxyContainer from "../../containers/DACProxy";
 
+// hooks
 import useMakerVaults from "./useMakerVaults";
 import useAllowVaultTransfer from "./useAllowVaultTransfer";
 import useImportVault from "./useImportVault";
 
 const ImportButton = () => {
+  const { address } = ConnectionContainer.useContainer();
   const { proxy } = DACProxyContainer.useContainer();
 
   // state
@@ -29,16 +35,45 @@ const ImportButton = () => {
   const closeModal = e => setIsOpen(false);
   const openModal = e => setIsOpen(true);
 
-  const buttonDisabled = !proxy || vaultIds.length === 0;
+  // case 1: not connected
+  if (!proxy) {
+    return <Button disabled>From MakerDAO</Button>;
+  }
 
+  // case 2: no vaults found on MakerDAO for this address
+  if (vaultIds.length === 0) {
+    return (
+      <Box>
+        <Button onClick={openModal}>From MakerDAO</Button>
+
+        <Modal isOpen={isOpen}>
+          <Card width={"640px"} p={0}>
+            <ModalCloseIcon onClick={closeModal} />
+
+            <Box p={4}>
+              <Heading.h3 mb="4">Import Vault from MakerDAO</Heading.h3>
+
+              <Text mb="3">
+                No Vaults were found on MakerDAO under your current address:
+              </Text>
+              <EthAddress address={address} />
+            </Box>
+            <ModalBottom>
+              <Button.Outline onClick={closeModal}>Close</Button.Outline>
+            </ModalBottom>
+          </Card>
+        </Modal>
+      </Box>
+    );
+  }
+
+  // case 3: vaults are found
   return (
     <Box>
-      <Button onClick={openModal} disabled={buttonDisabled}>
-        From MakerDAO
-      </Button>
+      <Button onClick={openModal}>From MakerDAO</Button>
 
       <Modal isOpen={isOpen}>
-        <Card width={"420px"} p={0}>
+        <Card width={"640px"} p={0}>
           <ModalCloseIcon onClick={closeModal} />
 
           <Box p={4}>
@@ -70,7 +105,7 @@ const ImportButton = () => {
           </Box>
 
           <ModalBottom>
-            <Button.Outline onClick={closeModal}>Cancel</Button.Outline>
+            <Button.Outline onClick={closeModal}>Close</Button.Outline>
             <Button ml={3} onClick={importVault}>
               Import
             </Button>
