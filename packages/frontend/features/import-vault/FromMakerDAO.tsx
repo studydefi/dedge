c914a1d32116f8f0602f1f9dc9b74d4ae5e47f6e
Adrian Li <li.adrianmc@gmail.com>
2020-04-01 22:24:01 -0400

    add loading spinners for allow and import

--- packages/frontend/features/import-vault/FromMakerDAO.tsx
+++ packages/frontend/features/import-vault/FromMakerDAO.tsx
@@ -1,5 +1,15 @@
 import { useState } from "react";
-import { Box, Modal, Text, Button, Heading, Card, EthAddress } from "rimble-ui";
+import {
+  Box,
+  Flex,
+  Loader,
+  Modal,
+  Text,
+  Button,
+  Heading,
+  Card,
+  EthAddress,
+} from "rimble-ui";
 
 // components
 import Select from "../../components/Select";
@@ -10,35 +20,40 @@ import ConnectionContainer from "../../containers/Connection";
 import DACProxyContainer from "../../containers/DACProxy";
 
 // hooks
-import useMakerVaults from "./useMakerVaults";
 import useAllowVaultTransfer from "./useAllowVaultTransfer";
 import useImportVault from "./useImportVault";
+import VaultsContainer from "../../containers/Vaults";
 
 const ImportButton = () => {
   const { address } = ConnectionContainer.useContainer();
   const { hasProxy } = DACProxyContainer.useContainer();
+  const { vaultIds } = VaultsContainer.useContainer();
 
   // state
   const [isOpen, setIsOpen] = useState(false);
   const [selectedVaultId, setSelectedVaultId] = useState(null);
 
   // hooks
-  const { vaultIds } = useMakerVaults();
-  const { allow } = useAllowVaultTransfer(selectedVaultId);
-  const { importVault, canImportVault, getCanImportVault } = useImportVault(selectedVaultId);
+  const { allow, loading: loadingAllow, importAllowed } = useAllowVaultTransfer(
+    selectedVaultId,
+  );
+  const { importVault, loading: loadingImport } = useImportVault(
+    selectedVaultId,
+  );
 
   // select first vault by default
   if (selectedVaultId === null && vaultIds.length > 0) {
     setSelectedVaultId(vaultIds[0]);
-    getCanImportVault(vaultIds[0]);
   }
 
-  const closeModal = e => setIsOpen(false);
-  const openModal = e => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
 
   // case 1: not connected
   if (!hasProxy) {
-    return <Button.Outline disabled>Import position from MakerDAO</Button.Outline>;
+    return (
+      <Button.Outline disabled>Import position from MakerDAO</Button.Outline>
+    );
   }
 
   // case 2: no vaults found on MakerDAO for this address
@@ -71,7 +86,7 @@ const ImportButton = () => {
   // case 3: vaults are found
   return (
     <Box>
-      <Button onClick={openModal}>From MakerDAO</Button>
+      <Button onClick={openModal}>Import position from MakerDAO</Button>
 
       <Modal isOpen={isOpen}>
         <Card width={"640px"} p={0}>
@@ -83,11 +98,7 @@ const ImportButton = () => {
             <Box mb="4">
               <Heading.h5 mb="2">1. Select your Vault</Heading.h5>
               <Select
-                onChange={x => {
-                  const selectedVaultId = x.target.value
-                  setSelectedVaultId(selectedVaultId);
-                  getCanImportVault(selectedVaultId);
-                }}
+                onChange={x => setSelectedVaultId(x.target.value)}
                 value={selectedVaultId}
                 required
               >
@@ -103,14 +114,36 @@ const ImportButton = () => {
 
             <Box>
               <Heading.h5 mb="2">2. Allow Vault transfer</Heading.h5>
-              <Button onClick={allow}>Allow</Button>
+              {importAllowed ? (
+                <Text>Import Approved</Text>
+              ) : (
+                <Button onClick={allow}>
+                  {loadingAllow ? (
+                    <Flex alignItems="center">
+                      <span>Allowing...</span> <Loader color="white" ml="2" />
+                    </Flex>
+                  ) : (
+                    "Allow Import"
+                  )}
+                </Button>
+              )}
             </Box>
           </Box>
 
           <ModalBottom>
             <Button.Outline onClick={closeModal}>Close</Button.Outline>
-            <Button disabled={!canImportVault} ml={3} onClick={importVault}>
-              Import
+            <Button
+              disabled={!importAllowed || loadingImport}
+              ml={3}
+              onClick={importVault}
+            >
+              {loadingImport ? (
+                <Flex alignItems="center">
+                  <span>Importing...</span> <Loader color="white" ml="2" />
+                </Flex>
+              ) : (
+                "Import Vault"
+              )}
             </Button>
           </ModalBottom>
         </Card>
--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -7,19 +7,21 @@ import Contracts from "../containers/Contracts";
 import DACProxy from "../containers/DACProxy";
 import BorrowBalances from "../containers/CompoundPositions";
 import CoinsContainer from "../containers/Coins";
+import VaultsContainer from "../containers/Vaults";
 
 const customTheme = {
   ...theme,
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
 };
-
 const WithProviders = ({ children }) => (
   <CoinsContainer.Provider>
     <Connection.Provider>
       <ToastMessage.Provider ref={node => (window.toastProvider = node)} />
       <Contracts.Provider>
         <DACProxy.Provider>
-          <BorrowBalances.Provider>{children}</BorrowBalances.Provider>
+          <VaultsContainer.Provider>
+            <BorrowBalances.Provider>{children}</BorrowBalances.Provider>
+          </VaultsContainer.Provider>
         </DACProxy.Provider>
       </Contracts.Provider>
     </Connection.Provider>
