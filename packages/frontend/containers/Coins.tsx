e6b9f81eae54c37754658e776c184e1158cb1cac
Kendrick Tan <kendricktan0814@gmail.com>
2020-04-04 17:36:16 +1000

    hooked up logic

--- packages/frontend/containers/Coins.tsx
+++ packages/frontend/containers/Coins.tsx
@@ -1,24 +1,64 @@
 import { createContainer } from "unstated-next";
+import { getLegos, networkIds } from "money-legos/dist";
+
+const legos = getLegos(networkIds.mainnet);
 
 const COINS = {
-  eth: { name: "Ether", symbol: "ETH", icon: "Eth", stable: false },
+  eth: {
+    name: "Ether",
+    symbol: "ETH",
+    icon: "Eth",
+    stable: false,
+    cTokenEquilaventAddress: legos.compound.cEther.address,
+  },
   bat: {
     name: "Basic Attention Token",
     symbol: "BAT",
     icon: "Bat",
     stable: false,
+    cTokenEquilaventAddress: legos.compound.cBAT.address,
   },
-  dai: { name: "Dai", symbol: "DAI", icon: "Dai", stable: true },
-  usdc: { name: "USD Coin", symbol: "USDC", icon: "Usd", stable: true },
-  rep: { name: "Augur", symbol: "REP", icon: "Rep", stable: false },
-  zrx: { name: "0x", symbol: "ZRX", icon: "Zrx", stable: false },
-  wbtc: { name: "Wrapped BTC", symbol: "WBTC", icon: "Btc", stable: false },
+  dai: {
+    name: "Dai",
+    symbol: "DAI",
+    icon: "Dai",
+    stable: true,
+    cTokenEquilaventAddress: legos.compound.cDAI.address,
+  },
+  usdc: {
+    name: "USD Coin",
+    symbol: "USDC",
+    icon: "Usd",
+    stable: true,
+    cTokenEquilaventAddress: legos.compound.cUSDC.address,
+  },
+  rep: {
+    name: "Augur",
+    symbol: "REP",
+    icon: "Rep",
+    stable: false,
+    cTokenEquilaventAddress: legos.compound.cREP.address,
+  },
+  zrx: {
+    name: "0x",
+    symbol: "ZRX",
+    icon: "Zrx",
+    stable: false,
+    cTokenEquilaventAddress: legos.compound.cZRX.address,
+  },
+  wbtc: {
+    name: "Wrapped BTC",
+    symbol: "WBTC",
+    icon: "Btc",
+    stable: false,
+    cTokenEquilaventAddress: legos.compound.cWBTC.address,
+  },
 };
 
 function useCoins() {
   const coinsArray = Object.entries(COINS).map(([k, v]) => ({ ...v, key: k }));
-  const stableCoins = coinsArray.filter(x => x.stable === true);
-  const volatileCoins = coinsArray.filter(x => x.stable === false);
+  const stableCoins = coinsArray.filter((x) => x.stable === true);
+  const volatileCoins = coinsArray.filter((x) => x.stable === false);
 
   return { COINS, stableCoins, volatileCoins };
 }
--- packages/frontend/features/position/BorrowCoin.tsx
+++ packages/frontend/features/position/BorrowCoin.tsx
@@ -1,18 +1,19 @@
-import {
-  Button,
-  Modal,
-  Card,
-  Box,
-  Text,
-  Field,
-  Input,
-  Heading,
-} from "rimble-ui";
+import { dedgeHelpers } from "../../../smart-contracts/dist/helpers";
+import { ethers } from "ethers";
+
+import { Button, Loader, Box, Flex, Field, Input } from "rimble-ui";
+
+import ContractsContainer from "../../containers/Contracts";
 import DACProxyContainer from "../../containers/DACProxy";
+
 import { useState } from "react";
 
 const BorrowCoin = ({ coin }) => {
+  const { contracts } = ContractsContainer.useContainer();
+  const { proxy } = DACProxyContainer.useContainer();
+  const [loading, setLoading] = useState(false);
   const [amount, setAmount] = useState("");
+
   return (
     <Box>
       {/* <Heading.h5 mb="2">Supply {coin.symbol}</Heading.h5> */}
@@ -23,11 +24,49 @@ const BorrowCoin = ({ coin }) => {
             required={true}
             placeholder="1337"
             value={amount}
-            onChange={e => setAmount(e.target.value.toString())}
+            onChange={(e) => setAmount(e.target.value.toString())}
           />
         </Field>
       </Box>
-      <Button>Borrow</Button>
+      <Button
+        ml={3}
+        disabled={loading}
+        onClick={async () => {
+          setLoading(true);
+
+          const { dedgeCompoundManager } = contracts;
+          const tx = await dedgeHelpers.compound.borrowThroughProxy(
+            proxy,
+            dedgeCompoundManager.address,
+            coin.cTokenEquilaventAddress,
+            ethers.utils.parseUnits(amount, coin.symbol === "USDC" ? 6 : 18)
+          );
+          window.toastProvider.addMessage(`Borrowing ${coin.symbol}...`, {
+            secondaryMessage: "Check progress on Etherscan",
+            actionHref: `https://etherscan.io/tx/${tx.hash}`,
+            actionText: "Check",
+            variant: "processing",
+          });
+          await tx.wait();
+
+          window.toastProvider.addMessage(
+            `Successfully borrowed ${coin.symbol}!`,
+            {
+              variant: "success",
+            }
+          );
+
+          setLoading(false);
+        }}
+      >
+        {loading ? (
+          <Flex alignItems="center">
+            <span>Borrowing...</span> <Loader color="white" ml="2" />
+          </Flex>
+        ) : (
+          "Borrow"
+        )}
+      </Button>
     </Box>
   );
 };
